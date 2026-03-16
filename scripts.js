1bd87176d045553f0fcfb8e219fbacfce54be3c7
neoyanis <neoyanis105000@proton.me>
2026-03-16 14:06:45 +0000

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function showProjects() {
+  document.getElementById('projects').classList.add('active');
+  document.getElementById('explore').classList.remove('active');
+}
+
+function showExplore() {
+  document.getElementById('explore').classList.add('active');
+  document.getElementById('projects').classList.remove('active');
+}
